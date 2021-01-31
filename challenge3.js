48b1c189139854b3c2df5573080a8ae466514c78
cliffchang1 <cliffchang@pacificamerican.org>
2021-02-01 02:52:28 +0800

    reset button setup

--- challenge3.js
+++ challenge3.js
@@ -3,41 +3,48 @@ var visitorscore = 0
 var period = 0
 let penalty1
 let penalty2
-function addhomescore(){
+function addHomeScore(){
     homescore = homescore + 1
     document.querySelector(".home-score").innerHTML = homescore
    }
-function deducthomescore(){ if(homescore > 0)
+function deductHomeScore(){ if(homescore > 0)
     {homescore = homescore- 1}
     document.querySelector(".home-score").innerHTML = homescore
    }
-   function addvisitorscore(){
+   function addVisitorScore(){
     visitorscore = visitorscore + 1
     document.querySelector(".guest-score").innerHTML = visitorscore
    }
-function deductvisitorscore(){if(visitorscore > 0)
+function deductVisitorScore(){if(visitorscore > 0)
     {visitorscore = visitorscore - 1}
     document.querySelector(".guest-score").innerHTML = visitorscore
    }
-function addhomepenalty(){
+function addHomePenalty(){
     penalty1 = prompt("Who is going to jail?")
     document.querySelector(".player-1").innerHTML = penalty1
 }
-function deletehomepenalty(){
+function deleteHomePenalty(){
     document.querySelector(".player-1").innerHTML = ""
 }
-function addvisitorpenalty(){
+function addVisitorPenalty(){
     penalty2 = prompt("Who is going to jail?")
     document.querySelector(".player-2").innerHTML = penalty2
 }
-function deletevisitorpenalty(){
+function deleteVisitorpenalty(){
     document.querySelector(".player-2").innerHTML = ""
 }
-function addperiod(){if(period < 3)
+function addPeriod(){if(period < 3)
     {period = period + 1}
     document.querySelector(".period-block").innerHTML = period
 }
-function deductperiod(){if(period > 1)
+function deductPeriod(){if(period > 1)
     {period = period - 1}
     document.querySelector(".period-block").innerHTML = period
+}
+function reset(){
+    document.querySelector(".home-score").innerHTML = ""
+    document.querySelector(".guest-score").innerHTML = ""
+    document.querySelector(".period-block").innerHTML = ""
+    document.querySelector(".player-1").innerHTML = ""
+    document.querySelector(".player-2").innerHTML = ""
 }
